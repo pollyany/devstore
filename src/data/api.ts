296590fd1985ea0d15eebeab08ca932937221849
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-02-15 11:36:12 -0300

    Fetch de produtos na Home

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,7 +2,8 @@ import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
-  const url = new URL(path, baseUrl)
+  const apiPrefixed = '/api'
+  const url = new URL(apiPrefixed.concat(path), baseUrl)
 
   return fetch(url, init)
 }
